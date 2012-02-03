0678167daee756289421f1d19ac9f1feee39e11f
daveshine <daveshine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-03 20:51:52 +0000

    Tagged version 1.2 of the plugin - major improvement - added new features and improvements ... :-)
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-toolbar/tags/1.2@499948 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ gftb-plugins.php
@@ -0,0 +1,330 @@
+<?php
+/**
+ * Display links to active Gravity Forms third-party add-ons/ plugins/ extensions settings' pages
+ *
+ * @package    Gravity Forms Toolbar
+ * @subpackage Plugin/Extension Support
+ * @author     David Decker - DECKERWEB
+ * @copyright  Copyright 2012, David Decker - DECKERWEB
+ * @license    http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
+ * @link       http://genesisthemes.de/en/wp-plugins/gravity-forms-toolbar/
+ * @link       http://twitter.com/#!/deckerweb
+ *
+ * @since 1.0
+ * @version 1.0
+ */
+
+/**
+ * Pronamic iDEAL settings pages (free, by Pronamic)
+ *
+ * @since 1.0
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'pronamic-ideal/pronamic-ideal.php' ) ) || class_exists( 'Pronamic_IDeal_IDeal' ) ) {
+
+	// Entry at "Add-Ons" level submenu
+	$menu_items['gftpaopideal'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'iDEAL Feeds', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_pronamic_ideal' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'iDEAL Feeds', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaopideal-newfeed'] = array(
+		'parent' => $gftpaopideal,
+		'title'  => __( 'Add new Feed', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_pronamic_ideal&view=edit' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Add new Feed', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaopideal-settings'] = array(
+		'parent' => $gftpaopideal,
+		'title'  => __( 'Pronamic iDEAL Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=iDEAL' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Pronamic iDEAL Settings', 'gravity-forms-toolbar' ) )
+	);
+
+	// Entries at "Extensions" level submenu
+	$menu_items['extpideal'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'Pronamic iDEAL Payment Gateway', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=pronamic_ideal' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Pronamic iDEAL Payment Gateway', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-feeds'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'iDEAL Feeds', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_pronamic_ideal' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'iDEAL Feeds', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-newfeed'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'Add new Feed', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_pronamic_ideal&view=edit' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Add new Feed', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-gfsettings'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'Pronamic iDEAL Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=iDEAL' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Pronamic iDEAL Settings', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-payments'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'Payments', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=pronamic_ideal_payments' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Payments', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-settings'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=pronamic_ideal_settings' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Settings', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-pages-generator'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'Pages Generator', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=pronamic_ideal_pages_generator' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Pages Generator', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-variants'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'Variants', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=pronamic_ideal_variants' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Variants', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['ext-pideal-documentation'] = array(
+		'parent' => $extpideal,
+		'title'  => __( 'Pronamic iDEAL Documentation', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=pronamic_ideal_documentation' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Pronamic iDEAL Documentation', 'gravity-forms-toolbar' ) )
+	);
+
+}  // end-if Pronamic iDEAL
+
+
+/**
+ * Gravity Forms Directory & Addons (free, by Katz Web Services, Inc.)
+ *
+ * @since 1.0
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'gravity-forms-addons/gravity-forms-addons.php' ) ) || class_exists( 'GFDirectory' ) ) {
+
+	// Entry at "Add-Ons" level submenu
+	$menu_items['gftpaogfd'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'Gravity Forms Directory & Addons', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=Directory+%26+Addons' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Gravity Forms Directory & Addons', 'gravity-forms-toolbar' ) )
+	);
+
+	// Entry at "Extensions" level submenu
+	$menu_items['ext-gfdirectory'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'Gravity Forms Directory & Addons', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=Directory+%26+Addons' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Gravity Forms Directory & Addons', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if GF Directory
+
+
+/**
+ * Gravity Forms Salesforce Add-on (free, by Katz Web Services, Inc.)
+ *
+ * @since 1.0
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'gravity-forms-salesforce/salesforce.php' ) ) || class_exists( 'GFSalesforce' ) ) {
+
+	// Entry at "Add-Ons" level submenu
+	$menu_items['gftpaogfsf'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'Gravity Forms Salesforce Add-On', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_salesforce' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Gravity Forms Salesforce Add-On', 'gravity-forms-toolbar' ) )
+	);
+
+	// Entry at "Extensions" level submenu
+	$menu_items['ext-gfsalesforce'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'Gravity Forms Salesforce Add-On', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_salesforce' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Gravity Forms Salesforce Add-On', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if GF Salesforce
+
+
+/**
+ * Gravity Forms Contact Form 7 Importer (free, by Katz Web Services, Inc.)
+ *
+ * @since 1.0
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'contact-form-7-gravity-forms/contact-form-7-gravity-forms.php' ) ) || class_exists( 'GFCF7_Import' ) ) {
+
+	// Entry at "Extensions" level submenu
+	$menu_items['ext-gfcf7import'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'Contact Form 7 Gravity Forms Importer', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gfcf7' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Contact Form 7 Gravity Forms Importer', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if GF CF7-Importer
+
+
+/**
+ * Gravity Forms ShootQ Add-On (free, by pussycatdev)
+ *
+ * @since 1.1
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'gravity-forms-shootq-add-on/shootq.php' ) ) || class_exists( 'GFShootQ' ) ) {
+
+	// Entry at "Add-Ons" level submenu
+	$menu_items['gftpaoshootq'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'ShootQ Feeds', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_shootq' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'ShootQ Feeds', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaoshootq-newfeed'] = array(
+		'parent' => $gftpaoshootq,
+		'title'  => __( 'Add new Feed', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_shootq&view=edit&id=0' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Add new Feed', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaoshootq-settings'] = array(
+		'parent' => $gftpaoshootq,
+		'title'  => __( 'ShootQ Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=ShootQ' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'ShootQ Settings', 'gravity-forms-toolbar' ) )
+	);
+
+	// Entry at "Extensions" level submenu
+	$menu_items['ext-gfshootq'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'ShootQ Add-On Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=ShootQ' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'ShootQ Add-On Settings', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if GF ShootQ
+
+
+/**
+ * Gravity Forms iContact Add-On (free, by Katz Web Services, Inc.)
+ *
+ * @since 1.1
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'gravity-forms-icontact/icontact.php' ) ) || class_exists( 'GFiContact' ) ) {
+
+	// Entry at "Add-Ons" level submenu
+	$menu_items['gftpaoicontact'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'iContact Feeds', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_icontact' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'iContact Feeds', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaoicontact-newfeed'] = array(
+		'parent' => $gftpaoicontact,
+		'title'  => __( 'Add new Feed', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_icontact&view=edit&id=0' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Add new Feed', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaoicontact-settings'] = array(
+		'parent' => $gftpaoicontact,
+		'title'  => __( 'iContact Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=iContact' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'iContact Settings', 'gravity-forms-toolbar' ) )
+	);
+
+	// Entry at "Extensions" level submenu
+	$menu_items['ext-gficontact'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'iContact Add-On Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=iContact' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'iContact Add-On Settings', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if GF iContact
+
+
+/**
+ * Gravity Forms Mad Mimi Add-On (free, by Katz Web Services, Inc.)
+ *
+ * @since 1.1
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'gravity-forms-mad-mimi/madmimi.php' ) ) || class_exists( 'GFMadMimi' ) ) {
+
+	// Entry at "Add-Ons" level submenu
+	$menu_items['gftpaomadmimi'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'Mad Mimi Feeds', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_madmimi' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Mad Mimi Feeds', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaomadmimi-newfeed'] = array(
+		'parent' => $gftpaomadmimi,
+		'title'  => __( 'Add new Feed', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_madmimi&view=edit&id=0' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Add new Feed', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaomadmimi-settings'] = array(
+		'parent' => $gftpaomadmimi,
+		'title'  => __( 'Mad Mimi Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=Mad+Mimi' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Mad Mimi Settings', 'gravity-forms-toolbar' ) )
+	);
+
+	// Entry at "Extensions" level submenu
+	$menu_items['ext-gfmadmimi'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'Mad Mimi Add-On Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=Mad+Mimi' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Mad Mimi Add-On Settings', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if GF Mad Mimi
+
+
+/**
+ * Gravity Forms ExactTarget Add-On (free, by Katz Web Services, Inc.)
+ *
+ * @since 1.1
+ */
+if ( ( function_exists( 'is_plugin_active' ) && is_plugin_active( 'gravity-forms-exacttarget/exacttarget.php' ) ) || class_exists( 'GFExactTarget' ) ) {
+
+	// Entry at "Add-Ons" level submenu
+	$menu_items['gftpaoexacttarget'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'ExactTarget Feeds', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_exacttarget' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'ExactTarget Feeds', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaoexacttarget-newfeed'] = array(
+		'parent' => $gftpaoexacttarget,
+		'title'  => __( 'Add new Feed', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_exacttarget&view=edit&id=0' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Add new Feed', 'gravity-forms-toolbar' ) )
+	);
+	$menu_items['gftpaoexacttarget-settings'] = array(
+		'parent' => $gftpaoexacttarget,
+		'title'  => __( 'ExactTarget Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=ExactTarget' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'ExactTarget Settings', 'gravity-forms-toolbar' ) )
+	);
+
+	// Entry at "Extensions" level submenu
+	$menu_items['ext-gfexacttarget'] = array(
+		'parent' => $extensions,
+		'title'  => __( 'ExactTarget Add-On Settings', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_settings&addon=ExactTarget' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'ExactTarget Add-On Settings', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if GF ExactTarget
+
+
+/**
+ * Placing of "Purchase more official Add-Ons" as very last entry of "Add-On" first-level menu
+ *
+ * @since 1.0
+ */
+if ( current_user_can( 'gravityforms_view_addons' ) || current_user_can( 'gform_full_access' ) ) {
+	$menu_items['gfaddonsinstalled-purchase'] = array(
+		'parent' => $gfaddonsinstalled,
+		'title'  => __( 'Purchase more Add-Ons', 'gravity-forms-toolbar' ),
+		'href'   => admin_url( 'admin.php?page=gf_addons' ),
+		'meta'   => array( 'target' => '', 'title' => __( 'Purchase more Add-Ons', 'gravity-forms-toolbar' ) )
+	);
+}  // end-if purchase more add-ons
---
+++ readme.txt
@@ -0,0 +1,162 @@
+=== Gravity Forms Toolbar ===
+Contributors: daveshine, gdragon
+Donate link: http://genesisthemes.de/en/donate/
+Tags: toolbar, adminbar, admin bar, gravity forms, gravityforms, forms, add-ons, addons, administration, resources, links, deckerweb
+Requires at least: 3.1
+Tested up to: 3.3.1
+Stable tag: 1.2
+
+This plugin adds useful admin links and resources for Gravity Forms to the WordPress Toolbar / Admin Bar.
+
+== Description ==
+
+This small and lightweight plugin just adds a lot Gravity Forms related resources to your toolbar / admin bar. Also links to all admin settings pages pages of the plugin are included, making the life of form administrators/ developers a lot easier. So you might just switch from the fontend of your site to read current 'Entries' or just 'Add a new Form' etc. *How cool is that? :)* Also, support for all official and lots of third-party add-ons is included!
+
+As the name suggests this plugin is primarily intended towards website admins/ developers. Beside the forum, support links etc. the main plugin settings links will only appear if the current user has the appropiate rights/ capabillities for these. This just goes hand in hand with the very same user rights Gravity Forms itself uses to display its menu entries. So also Editors or any user user with the proper rights could view 'Entries' etc (you could easily tweak all those roles & caps with the awesome ["Members" plugin](http://wordpress.org/extend/plugins/members/)!).
+
+= NEW Features since v1.2 =
+* Admin settings page to optinally remove Support/Docs/FAQ and/or "Active Extensions" sections from menu (settings under "General Settings")
+* Admin settings allow also to dynamically add existing Forms (link to their edit pages) and Entries of these forms
+* Visual notification of new entries in the Toolbar (top level) and on "Entries" sub-level (if dynamic entries are activated)
+
+= Official Add-Ons Support =
+*The plugin out of the box supports links to settings pages of all officially available Gravity Forms Add-Ons, 7 to date:*
+
+* User Registration
+* PayPal
+* MailChimp
+* AWeber
+* Campaign Monitor
+* Freshbooks
+* Twilio
+
+= Add-Ons/ Plugin Support =
+*At this time the plugin out of the box supports also links to settings pages of some third-party Gravity Forms related plugins:*
+
+* Plugin: ["Gravity Forms Directory & Addons (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-addons/)
+* Plugin: ["Pronamic iDEAL" Payment Gateway (free, by Pronamic, NL)](http://wordpress.org/extend/plugins/pronamic-ideal/)
+* Plugin: ["Gravity Forms Salesforce Add-On (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-salesforce/)
+* Plugin: ["Gravity Forms iContact Add-On (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-icontact/)
+* Plugin: ["Gravity Forms Mad Mimi Add-On (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-mad-mimi/)
+* Plugin: ["Gravity Forms ExactTarget Add-On (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-exacttarget/)
+* Plugin: ["Gravity Forms ShootQ Add-On (free, by pussycatdev)](http://wordpress.org/extend/plugins/gravity-forms-shootq-add-on/)
+* Plugin: ["Gravity Forms Contact Form 7 Importer (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/contact-form-7-gravity-forms/)
+* Plugin: ["Members" (free, by Justin Tadlock)](http://wordpress.org/extend/plugins/members/)
+* *More third-party add-ons support will come with the next updates, so stay tuned :)*
+* *Your plugin? - [Just contact me with specific data](http://genesisthemes.de/en/contact/)*
+
+= Special Features =
+* Not only supporting official Gravity Forms/ Gravity Help sites ALSO third-party and user links - so just the whole Gravity Forms ecosystem :)
+* Link to downloadable German language packs - only displayed when German locales are active (de_DE, de_AT, de_CH, de_LU)
+* Link to Dutch language/translation plugin - only displayed when Dutch locales are active (nl_NL or nl)
+* *NOTE:* I would be happy to add more language/locale specific resources and more useful third-party links - just contact me!
+
+= Localization =
+* English (default) - always included
+* German - always included
+* .pot file (`gravity-forms-toolbar.pot`) for translators is also always included :)
+* *Your translation? - [Just send it in](http://genesisthemes.de/en/contact/)*
+
+Credit where credit is due: This plugin here is inspired and based on the work of Remkus de Vries @defries and his awesome "WooThemes Admin Bar Addition" plugin.
+
+This plugin is a co-work from:
+[David Decker - DECKERWEB from deckerweb.de and GenesisThemes](http://genesisthemes.de/en/)
+*and*
+[Milan Petrovic - Dev4Press from dev4press.com](http://dev4press.com/)
+
+= Feedback =
+* We are open for your suggestions and feedback - Thank you for using or trying out one of our plugins!
+* Drop us a line [@deckerweb](http://twitter.com/#!/deckerweb) or [@dev4press](http://twitter.com/#!/dev4press) on Twitter
+* Follow [DECKERWEB Facebook page](http://www.facebook.com/deckerweb.service) and [Dev4Press Facebook page](http://www.facebook.com/dev4press)
+* Or follow [+David Decker](http://deckerweb.de/gplus) on Google Plus ;-)
+
+= More =
+* [Also see my other plugins](http://genesisthemes.de/en/wp-plugins/) or see [my WordPress.org profile page](http://profiles.wordpress.org/users/daveshine/)
+* Tip: [*GenesisFinder* - Find then create. Your Genesis Framework Search Engine.](http://genesisfinder.com/)
+
+== Installation ==
+
+1. Upload the entire `gravity-forms-toolbar` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Look at your toolbar / admin bar and enjoy using the new Gravity Forms links there :)
+4. Adjust a few options on the settings page - under "General Settings" > "Gravity Forms Toolbar"
+5. Go and manage your entries and forms or develop new ones :)
+
+== Frequently Asked Questions ==
+
+= Does this plugin work with newest WP version and also older versions? =
+Yes, this plugin works really fine with WordPress 3.3 and 3.3.1!
+It also works great with WP 3.2 - and also should with WP 3.1 - but we only tested extensively with WP 3.3+ this time. So you always should run the latest WordPress version for a lot of reasons.
+
+= How are new resources being added to the toolbar / admin bar? =
+Just drop me a note on [my Twitter @deckerweb](http://twitter.com/deckerweb) or via my contact page and I'll add the link(s) if it is useful for admins/ webmasters and the Gravity Forms community.
+
+= How could my plugin/extension settings page be added to the admin bar links? =
+This is possible of course and highly welcomed! Just drop me a note on [my Twitter @deckerweb](http://twitter.com/deckerweb) or via my contact page and we sort out the details!
+Particularly, I need the admin url for the primary options page (like so `wp-admin/admin.php?page=foo`). I also need the correct name of the main PHP class or function (to check if the plugin extension exists or not). (I don't own all the premium stuff myself yet so you're more than welcomed to help me out with these things. Thank you!)
+
+= There are still some other plugins for Gravity Forms out there why aren't these included by default? =
+Simple answer: Linking/ adding is only possible where a plugin has its own admin pages and these are properly accessable.
+
+== Screenshots ==
+
+1. Gravity Forms Toolbar in default state (running with WordPress 3.3+ here)
+2. Gravity Forms Toolbar in action - primary level (running with WordPress 3.3+ here)
+3. Gravity Forms Toolbar in action - a secondary level (running with WordPress 3.3+ here)
+4. Gravity Forms Toolbar in action - a third level, for the official (and some third-party) Add-Ons (running with WordPress 3.3+ here)
+5. Gravity Forms Toolbar - Notification of new entries on top-level (running with WordPress 3.3+ here)
+6. Gravity Forms Toolbar - Little admin settings page (running with WordPress 3.3+ here)
+
+== Changelog ==
+
+= 1.2 =
+* Extended plugin functionality a lot, making it even more useful! - Thanx to new plugin co-author Milan Petrovic of Dev4Press!
+ * NEW: Admin settings page to optinally remove "Support/Docs/FAQ" and/or "Active Extensions" sections from menu (settings under "General Settings")
+ * NEW: Admin settings allow also to dynamically add existing "Forms" (link to their edit pages) and "Entries" of these forms
+ * NEW: Visual notification of new entries in the toolbar (top level) and on "Entries" sub-level (if dynamic entries are activated)
+* CODE: Improved conditional check for Dutch language plugin (only display link if plugin is not activated)
+* UPDATE: Extended and improved readme.txt file
+* UPDATE: Updated German translations and also the .pot file for all translators!
+
+= 1.1 =
+* Extended the third-party plugin support even more:
+ * NEW: Added link to user profile at Gravity Help - for even easier access to your forum favorites etc. (only displaying for admins)
+ * NEW: Added link to official blog post, documenting about "Gravity Forms CSS: Targeting Specific Elements" (Rocket Genius Blog)
+ * NEW: Added useful resource link for CSS styling (plugin "Gravity Forms CSS Ready Class Selector")
+ * NEW: Added plugin support for ["Gravity Forms iContact Add-On (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-icontact/)
+ * NEW: Added plugin support for ["Gravity Forms Mad Mimi Add-On (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-mad-mimi/)
+ * NEW: Added plugin support for ["Gravity Forms ExactTarget Add-On (free, by Katz Web Services, Inc.)](http://wordpress.org/extend/plugins/gravity-forms-exacttarget/)
+ * NEW: Added plugin support for ["Gravity Forms ShootQ Add-On (free, by pussycatdev)](http://wordpress.org/extend/plugins/gravity-forms-shootq-add-on/)
+ * NEW: Added plugin support for ["Members (free, by Justin Tadlock)](http://wordpress.org/extend/plugins/members/)
+* BUGFIX: Corrected variable for Dutch language plugin, making link display finally work :)
+* CODE: Minor tweaks and improvements.
+* UPDATE: Updated German translations and also the .pot file for all translators!
+* NEW: Added banner image on WordPress.org for better plugin branding :)
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.2 =
+Major improvement - Added little admin settings page to optionally remove unwanted entries. Added visual notifications for new form entries. New plugin co-author Milan Petrovic of Dev4Press - big thanks! Further, improved readme.txt file; made a few code tweaks. Also, updated .pot file for translators together with German translations. 
+
+= 1.1 =
+Several additions and improvements - Added 3 more useful user/resource links. Further, added plugin support for 5 more free plugins. Also, updated .pot file for translators together with German translations.
+
+= 1.0 =
+Just released into the wild.
+
+== Translations ==
+
+* English - default, always included
+* German: Deutsch - immer dabei! [Download auch via deckerweb.de](http://deckerweb.de/material/sprachdateien/gravityforms-und-addons/#gravity-forms-toolbar)
+
+*Note:* All my plugins are localized/ translateable by default. This is very important for all users worldwide. So please contribute your language to the plugin to make it even more useful. For translating I recommend the awesome ["Codestyling Localization" plugin](http://wordpress.org/extend/plugins/codestyling-localization/) and for validating the ["Poedit Editor"](http://www.poedit.net/).
+
+== Additional Info ==
+**Idea Behind / Philosophy:** Just a little leightweight plugin for all the form developers and managers out there working 
+with the incredible Gravity Forms to make their daily admin life a bit easier. I'll try to add more add-on/plugin support if it makes some sense. So stay tuned :).
+
+== Credits ==
+**Big thanks** to Milan Petrovic of Dev4Press who made the options panel for this plugin - so it's still lightweight but even more useful!
